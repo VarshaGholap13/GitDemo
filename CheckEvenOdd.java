32c7bfc0920e2e9a6384f2e65bc8620ee6b0aa2e
VarshaGholap13 <varshagholap2022@gmail.com>
2023-09-27 08:26:03 +0530

    First Commit

---
+++ CheckEvenOdd.java
@@ -0,0 +1,17 @@
+public class CheckEvenOdd
+{
+	public static void main(String[] args)
+	{
+		int number=13;
+		
+		
+		//Check if number divisible by 2
+		
+		if(number%2==0)
+		
+			System.out.println("Number is Even");
+		else
+			System.out.println("Number is Odd");
+	
+	}
+}
